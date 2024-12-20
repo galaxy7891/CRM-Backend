52a401af794dd595bcb2469bc06c7aaaa3a43878
Hernansandi <hernanlaksono@gmail.com>
2024-12-20 11:08:09 +0700

    cloudinary

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use App\Traits\HasUuid;
-use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Cloudinary\Cloudinary;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Cviebrock\EloquentSluggable\Sluggable;
